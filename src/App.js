d6781921adc43b56bd6bbf53cd0709756dae8adb
strogan <asdof01@gmail.com>
2024-04-29 17:42:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import DatePicker from "material-ui/DatePicker";
+import injectTapEventPlugin from "react-tap-event-plugin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+injectTapEventPlugin();
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedDate: null,
+      selectedDate2: null,
+      isSecondDateClicked: false,
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleDateChangeSecond = this.handleDateChangeSecond.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleDateChange(event, date) {
+    this.setState({ selectedDate: date });
+    this.setState({ selectedDate2: date });
+  }
+
+  handleDateChangeSecond(event, date) {
+    this.setState({ selectedDate2: date });
+  }
+
+  handleClick() {
+    this.setState({ isSecondDateClicked: true });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>First DatePicker:</p>
+        <DatePicker
+          hintText="Select Date"
+          value={this.state.selectedDate}
+          onChange={this.handleDateChange}
+        />
+        <p>Second DatePicker:</p>
+        <DatePicker
+          hintText="Select Date"
+          onChange={this.handleDateChangeSecond}
+          onClick={this.handleClick}
+          value={
+            this.state.isSecondDateClicked ? this.state.selectedDate2 : null
+          }
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
